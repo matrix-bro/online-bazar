6a8aa2f9af26d36e7d3978b114417ea4fea64a6c
matrix-bro <razmhz11@gmail.com>
2023-11-30 12:52:43 +0545

    Edit Item Details

--- app/views.py
+++ app/views.py
@@ -78,7 +78,15 @@ def delete_item(request, pk):
 def edit_item(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
 
-    form = EditItemForm()
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item-details', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
 
     return render(request, 'app/item_form.html', {
         'form': form,
